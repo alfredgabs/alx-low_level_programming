e304640b7d9322bc195e311a60cb9220000d256b
alfredgabs <alfredokperin@gmail.com>
2022-10-16 06:20:47 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,12 +10,13 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() -RAND_MAX / 2;
-	if(n == 0)
+
+	n = rand() - RAND_MAX / 2;
+	if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
-	else if(n > 5)
+	else if (n > 5)
 	{
 		printf("%d is greater\n", n);
 	}
@@ -26,4 +27,3 @@ int main(void)
 	return (0);
 
 }
-	
