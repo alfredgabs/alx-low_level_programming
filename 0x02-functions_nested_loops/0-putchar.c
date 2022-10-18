977555faaa0628bfc2e3613927bc80d66f735ce5
alfredgabs <alfredokperin@gmail.com>
2022-10-18 15:08:13 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
 /**
- * main - this function will print _putchar to the terminal.
+ * main - function that printa the _putchar
  *
- * Return: Alwyas 0 (success)
+ * Return: Always 0 (success)
  */
-int main(void)
+
+int main(void)
 {
-	_putchar ("_");
-	_putchar ("p");
-	_putchar ("u");
-	_putchar ("t");
-	_putchar ("c");
-	_putchar ("h");
-	_putchar ("a");
-	_putchar ("r");
-	_putchar ("\n");
-	return(0);
+	_putchar ('_');
+	_putchar ('p');
+	_putchar ('u');
+	_putchar ('t');
+	_putchar ('c');
+	_putchar ('h');
+	_putchar ('a');
+	_putchar ('r');
+	_putchar ('\n');
+	return (0);
 }
