ef19652eec7e82323f130c2baac9a7b9b942e539
alfredgabs <alfredokperin@gmail.com>
2022-10-19 09:14:48 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10 times and new line
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int n = 1;
+
+	for (n <= 10)
+	{
+		for (ch = 'a'; ch = 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+
+	}
+}
