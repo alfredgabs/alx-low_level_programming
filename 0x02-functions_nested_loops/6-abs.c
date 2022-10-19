c156cc4a7ae4dc18196e60a3c917e22468bba18c
alfredgabs <alfredokperin@gmail.com>
2022-10-19 15:01:13 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes a specific interger
+ * @n: a given value
+ * Return: Always 0
+ */
+int _abs(int)
+{
+	int n;
+
+	if (n > 0)
+		return (n);
+	else
+		return (-(n));
