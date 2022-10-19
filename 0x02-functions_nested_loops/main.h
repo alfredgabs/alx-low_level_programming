b78c0920a92302dfcad39d91f3fb9a47e3bc93cc
alfredgabs <alfredokperin@gmail.com>
2022-10-19 07:54:44 -0700

    task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 void print_alphabet_x10(void);
@@ -14,3 +14,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
+#endif
