8ac3732d9629e80afeb5d0337ce3a83b6313edf1
alfredgabs <alfredokperin@gmail.com>
2022-10-19 14:26:40 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sign - print sign
+ * @n: value
+ * Return: 1 if n is gretaer than 0, otherwise -1
+ */
+int print_sign(int n)
+{
+	int n;
+
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+
+	else if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+
+	else
+	{
+		return (-1);
+		_putchar('-');
+	}
+	_putchar('\n');
+}
