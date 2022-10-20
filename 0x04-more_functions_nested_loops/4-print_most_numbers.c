26012dd6636fc99a47dc0fa69e23ddb2dd843108
alfredgabs <alfredokperin@gmail.com>
2022-10-20 08:06:16 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,12 +7,13 @@
  */
 void print_most_numbers(void)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i <= 9; i++)
+	while (i <= 9)
 	{
 		if (i != 2 && i != 4)
 			_putchar(i + '0');
-		_putchar('\n');
+		i++;
 	}
+	_putchar('\n');
 }
