625520331c38fd577fd86bcc4684210a9a8309e8
alfredgabs <alfredokperin@gmail.com>
2022-10-21 04:40:34 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,13 +10,13 @@ void more_numbers(void)
 {
 	int i, num;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i <= 10; i++)
 	{
 		for (num = 0; num <= 14; num++)
 		{
-			if (num >= 10)
+			if (num > 10)
 			{
-				_putchar((num / 10) + 48);
+				_putchar('0');
 			}
 			_putchar((num % 10) + 48);
 		}
