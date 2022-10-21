59183c9189514ed00681ecd276fca3ae23223812
alfredgabs <alfredokperin@gmail.com>
2022-10-20 17:52:03 -0700

    task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -11,7 +10,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 100 != 0) || (year % 4 == 0 && year % 400 == 0))
+	if ((year % 4 == 0) || (year % 400 == 0 && year % 100 != 0))
 	{
 		if (month >= 3 && day >= 60)
 		{
