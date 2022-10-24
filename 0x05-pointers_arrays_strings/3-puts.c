126675607c77bfc4311eae5c4ff34dc439585908
alfredgabs <alfredokperin@gmail.com>
2022-10-24 11:45:12 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - print string to stdout
+ * @str: string to be printed
+ */
+void _puts(char *str)
+{
+	while (*s != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
