28860e1f721deec45c6d3c7df555d396b5893561
alfredgabs <alfredokperin@gmail.com>
2022-10-27 06:45:50 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,12 +11,12 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int j;
 
-	for (i = 0; dest[i] != '\0')
+	i = 0;
+	while (dest[i] != '\0')
 		i++;
-	for (j = 0; j < n && src[j] != '\0')
-	{
+	j = 0;
+	while (j < n && src[j] != '\0')
 		dest[i++] = src[j++];
-	}
 	dest[i] = '\0';
 	return (dest);
 }
